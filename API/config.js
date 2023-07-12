9df16a76b83ed74bb0008e292ffd18d91fbe8d68
camm2210 <camilodiaza37@gmail.com>
2023-07-12 14:04:02 -0500

    server created

---
+++ API/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const { FRONT, PORT } = process.env;
+
+module.exports = {
+  FRONT,
+  PORT,
+};
---
+++ API/src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes/index");
+const { FRONT } = require("../config");
+
+const server = express();
+server.name = "API";
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", FRONT);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.use("/", routes);
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
