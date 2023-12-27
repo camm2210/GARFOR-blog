b02c051c535f72a98c1d023e491d740344ea6893
camm2210 <camilodiaza37@gmail.com>
2023-12-27 07:37:57 -0500

    edit post

--- client/src/components/Components/Blogcard.tsx
+++ client/src/components/Components/Blogcard.tsx
@@ -13,7 +13,7 @@ function Blogcard(data: any) {
 
   return (
     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      {posts.map((post: Post) => (
+      {posts?.map((post: Post) => (
         <div
           key={post._id}
           className="flex max-w-xl flex-col items-start justify-between"
